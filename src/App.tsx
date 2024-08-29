81a8b49664e74914632dda4c5bd4d8fe555a4184
marko <markomarynovych@gmail.com>
2024-08-29 19:32:09 +0300

    Implemented Frontend Logic to Block Scout's Until Gender is Selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { NavBar } from "./components/layout/navbar"
+import { useDisclosure } from "@nextui-org/react"
 import Accordion from "./components/accordion/accordion"
-import { useUserStore } from "./lib/auth/useUser.tsx"
+import { NavBar } from "./components/layout/navbar"
+import GenderModal from "./components/modal/gender-modal/index.tsx"
 import AccordionUserComponent from "./components/user-accordion/accordion"
+import { useUserStore } from "./lib/auth/useUser.tsx"
+import { useEffect } from "react"
 
 function App() {
   const { user } = useUserStore((state) => ({
     user: state.user,
   }))
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+  useEffect(() => {
+    if (!user?.sex) {
+      onOpen()
+    }
+  }, [user?.sex, onOpen])
+
   return (
     <div>
+      <GenderModal userEmail={user?.email} isOpen={isOpen} onOpenChange={onOpenChange} />
       <NavBar />
       {user?.roles[0] === "FOREMAN" ? (
         <Accordion />
