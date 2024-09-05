8b6da1c0cc86121cbc9fca3fa329e3648e5f74e5
marko <markomarynovych@gmail.com>
2024-09-05 21:45:32 +0300

    fix(logo): add public route to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 import compression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: "public",
+  base: "./",
   plugins: [
     react(),
     compression({
