12bc08ea360b7c01f653f072c954c98d99714a4c
marko <markomarynovych@gmail.com>
2024-10-18 13:24:17 +0300

    fix(welcome messages): change text

--- src/components/common/foreman-info-message/index.tsx
+++ src/components/common/foreman-info-message/index.tsx
@@ -1,8 +1,7 @@
 const ForemanNextSteps = () => {
   return (
     <div className="max-w-[50rem] mt-6 flex items-start justify-center flex-col mx-auto w-[90%] space-y-6">
-      <h2 className="text-2xl font-bold text-center">Вітаю!</h2>
-      <p className="text-lg">
+      <p className="mt-8 text-lg">
         Схоже, що в тебе ще немає проб твоїх юнаків/юначок. Ти можеш їх добавити відкривши
         меню у правому верхньому куті
       </p>
--- src/components/common/next-steps-message/index.tsx
+++ src/components/common/next-steps-message/index.tsx
@@ -7,17 +7,19 @@ const NextSteps = ({ sex }: { sex: string }) => {
   }
 
   return (
-    <div className="max-w-[50rem] flex items-start justify-center flex-col mx-auto w-[90%] space-y-6">
-      <h2 className="text-2xl font-bold text-center mb-4">Вітаю!</h2>
-      <p className="text-lg">
-        Ти {messageText.got} на сайт онлайн проби. Якщо ти {messageText.scout},
-        то попроси {messageText.your_foreman}
-        добавити тебе в свій "гурток". Якщо ж ти {messageText.foreman}, то
-        сконтактуй з нами і ми все тобі пояснимо)
-      </p>
-      <span>Ось контакти:</span>
+    <div className="max-w-[50rem] mt-8 flex items-start justify-center flex-col mx-auto w-[90%] space-y-2">
+      <span className="text-lg">Ти {messageText.got} на сайт онлайн проби</span>
+      <span>
+        Якщо ти {messageText.scout}, то попроси {messageText.your_foreman}{" "}
+        добавити тебе в свій "гурток"
+      </span>
+      <span>
+        Якщо ж ти {messageText.foreman}, то сконтактуй з нами і ми все тобі
+        пояснимо)
+      </span>
+      <span>Ось пошта, на яку можете написати:</span>
       <ul className="list-disc list-inside space-y-2 text-lg">
-        <li>Контакти</li>
+        <li>office@lvivplast.org</li>
       </ul>
     </div>
   )
